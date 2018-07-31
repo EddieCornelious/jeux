cd95152e47d7f1273f522054095561c76dddb1df
Eddie Cornelious <eulerissmart@gmail.com>
2018-07-31 20:19:02 +0000

    added chain function for middleware

---
+++ src/chain.js
@@ -0,0 +1,20 @@
+
+var chainer = (starter, mw) => {
+  const args = [starter, ...mw];
+  const functions = [];
+
+  for (let i = 0; i < args.length; i++) {
+    functions[i] = args[i];
+  }
+
+  return (action) => {
+    return functions.reduce((value, func) => {
+      if (value === undefined) {
+        return (() => undefined)();
+      }
+      return func(value);
+    }, action);
+  };
+};
+
+export default chainer;
