a4f08dea928ac47e633abcbcdf8be6db3db0e0cf
Eddie Cornelious <eulerissmart@gmail.com>
2018-08-07 19:09:40 +0000

    added changed flag and compared prev value with new reducer value

--- src/updateStoreFromReducers.js
+++ src/updateStoreFromReducers.js
@@ -1,6 +1,7 @@
 
 function updateStoreFromReducers(state = {}, reducers, reducerKeys, action) {
   const injectedAction = action || '$__DEFAULTACTION$__';
+  let storeHasChanged = false;
 
   for (let i = 0; i < reducerKeys.length; i += 1) {
     const reducerKey = reducerKeys[i];
@@ -9,14 +10,20 @@ function updateStoreFromReducers(state = {}, reducers, reducerKeys, action) {
     if (typeof reducerFunc !== 'function') {
       throw new TypeError('Reducer keys must map to functions');
     }
+    const knownResult = state[reducerKey];
     const reducerRes = reducerFunc(state[reducerKey], injectedAction);
 
     if (reducerRes === undefined) {
       throw new TypeError('Reducer initial state cannot be undefined, try using a default arguments');
     }
+
+    if (knownResult !== reducerRes) {
+      storeHasChanged = true;
+    }
+
     state[reducerKey] = reducerRes;
   }
-  return state;
+  return storeHasChanged;
 }
 
 export default updateStoreFromReducers;
